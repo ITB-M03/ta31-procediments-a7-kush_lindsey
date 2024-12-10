417e0d63ba8f577ecde14c4b6320ab89c8b05161
Lindsey López <lindsey.antunez7e8@itb.cat>
2024-12-10 18:22:37 +0100

    eje4

--- src/main/kotlin/controllers/eje_4.kt
+++ src/main/kotlin/controllers/eje_4.kt
@@ -2,12 +2,42 @@ package org.example.controllers
 
 import java.util.*
 
+/**
+ * Programa que muestran un menu en el que da tres opciones las cuales consisten en agregar o eliminar un numero
+ * y mostar la lista donde se guardan los numeros introducidos
+ * @author Lindsey Antunez
+ * @version 1.0
+ * @param scan Scanner
+ * @param menu variable que guarda la seleccion del usuario en del menu
+ * @param pila donde se guarda la lista que se creo
+ */
 fun main() {
     var scan = abrirScanner()
     var menu = menu(scan)
-    var pila = crearArray()
+    var pila = crearLista()
+    while (menu != 4){
+        if (menu ==1){
+            print("Introduzca el numero que desea añadir: ")
+            var num = scan.nextInt()
+            añadirNumero(pila, num)
+        }
+        else if (menu == 2){
+            eliminarNumero(pila)
+        }
+        else if (menu ==3){
+            muestraLista(pila)
+        }
+        menu= menu(scan)
+    }
     cerrarScanner(scan)
 }
+
+/**
+ * Menu donde se muestran las opciones que ofrece el programa
+ * @author Lindsey Antunez
+ * @return seleccion variable de tipo entero que guarda la opcion que selecciono el usuario.
+ *
+ */
 fun menu(scan: Scanner):Int {
     var seleccion : Int
     print("""
@@ -23,16 +53,56 @@ fun menu(scan: Scanner):Int {
     seleccion = scan.nextInt()
     return seleccion
 }
-fun crearArray(){
-    var array = emptyArray<Int>()
+
+/**
+ * Funcion que crea la lista en la que se guardaran los numero
+ * @author Lindsey Antunez
+ * @return lista Es la lista vacia en la que se introduciran los numeros
+ */
+fun crearLista(): MutableList<Int> {
+    var lista = emptyList<Int>().toMutableList()
+    return lista
 }
 
-fun añadirNumero(pila:Array<Int>){
+/**
+ * Funcion con la que se añade un numero a la lista
+ * @author Lindsey Antunez
+ * @param pila lista donde añadira el numero
+ * @param num numero que va a añadir
+ */
+fun añadirNumero(pila:MutableList<Int>, num:Int){
     if (pila.size == 10){
         println("La pila esta llena")
     }
     else{
+        pila.add(num)
+    }
+}
 
+/**
+ * Funcion que elimina el ultimo numero que se introdujo, en el caso de que este vacia muestra
+ * un mensaje informando que esta vacia y no se puede eliminar ningun numero
+ * @author Lindsey Antunez
+ * @param pila lista de donde eliminara el numero
+ */
+fun eliminarNumero(pila:MutableList<Int>){
+    if (pila.isEmpty()){
+        println("La pila esta vacia, no puede eliminar ningun numero.")
+    }
+    var numeroBorrado = pila[pila.size-1]
+    pila.removeAt((pila.size-1))
+    println("Se borro el numero $numeroBorrado")
+    println(pila)
+}
+
+/**
+ * Funcion que muestra la lista empezando por el ultimo numero hasta el primero
+ * @author Lindsey Antunez
+ * @param pila lista que se muestra por pantalla
+ */
+fun muestraLista(pila: MutableList<Int>){
+    for (i in pila.size-1 downTo 0){
+        print("${pila[i]}, ")
     }
 }
 
