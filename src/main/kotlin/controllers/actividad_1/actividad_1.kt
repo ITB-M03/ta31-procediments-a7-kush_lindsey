6c082fcc9639ee1d7e4192195f44780d85bf47f3
khushbir.kumar.7e8 <khushbir.kumar.7e8@itb.cat>
2024-12-13 11:39:48 +0100

    historia

--- src/main/kotlin/controllers/actividad_1/actividad_1.kt
+++ src/main/kotlin/controllers/actividad_1/actividad_1.kt
@@ -1,14 +1,16 @@
 package controllers.actividad_1
 
-import controllers.actividad_3.mostrarResultado
+import controllers.actividad_3.finalscan
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
+
 fun main(){
     val scan = inicioscan()
-    pedirFecha(scan)
-    pedirTipoIva(scan)
-    pedirPrecio(scan)
+    val preu = pedirPrecio(scan)
+    val tipus = pedirTipoIva(scan)
+    val data = pedirFecha(scan)
+    calcularIva(preu,tipus, data)
     finalscan(scan)
 }
 
@@ -35,122 +37,59 @@ fun pedirTipoIva (scan: Scanner):String{
     return tipoIva
 }
 
-fun calcularIva (numero:Float, dia:Int, mes:Int, anyo:Int, tipoIva:String){
-    val ivaGeneral :Float
-    val ivaReduido : Float
-    val ivaSuperreduido : Float
-    val ivaExento : Float
-    if (dia in 1..31 && mes in 1..12 && anyo in 1986..1992){
-        if (tipoIva == "general"){
-            ivaGeneral = numero * 0.12f
-            println("${ivaGeneral+numero}")
+fun calcularIva (preu:Float, tipus:String, data: LocalDate) {
+    val resultado: Float
+    val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+    when (tipus) {
+        "Exent" -> println(preu)
+        "superreduit" -> if (data in LocalDate.parse("01-01-1986", formatter)..LocalDate.parse(
+                "01-01-1992", formatter)) {
+            println("NO EXISTEIX")
+        } else if (data in LocalDate.parse("01-01-1993", formatter)..LocalDate.parse("01-01-1995", formatter)) {
+            resultado = preu * 0.03f
+            println(resultado)
+        } else if (data in LocalDate.parse("01-01-1995", formatter)..LocalDate.parse("01-01-3000", formatter)) {
+            resultado = preu * 0.04f
+            println(resultado + preu)
+        } else {
+            println("ERROR")
         }
-        if (tipoIva == "reduida"){
-            ivaReduido = numero * 0.06f
-            println("${ivaReduido+numero}")
-        }
-        if (tipoIva == "superreduit"){
-            println("No Existeix")
-        }
-        if (tipoIva == "exento"){
-            ivaExento = numero
-            println("${ivaExento+numero}")
-        }
-    }
-    else if (dia in 1..31 && mes in 1..12 && anyo in 1992..1993){
-        if (tipoIva == "general"){
-            ivaGeneral = numero * 0.15f
-            println("${ivaGeneral+numero}")
-        }
-        if (tipoIva == "reduida"){
-            ivaReduido = numero * 0.06f
-            println("${ivaReduido+numero}")
-        }
-        if (tipoIva == "superreduit"){
-            println("No Existeix")
-        }
-        if (tipoIva == "exento"){
-            ivaExento = numero
-            println("${ivaExento+numero}")
-        }
-    }
-    else if (dia in 1..31 && mes in 1..12 && anyo in 1993..1995){
-        if (tipoIva == "general"){
-            ivaGeneral = numero * 0.15f
-            println("${ivaGeneral+numero}")
-        }
-        if (tipoIva == "reduida"){
-            ivaReduido = numero * 0.06f
-            println("${ivaReduido+numero}")
-        }
-        if (tipoIva == "superreduit"){
-            ivaSuperreduido = numero * 0.03f
-            println("${ivaSuperreduido+numero}")
-        }
-        if (tipoIva == "exento"){
-            ivaExento = numero
-            println("${ivaExento+numero}")
-        }
-    }
-    else if (dia in 1..31 && mes in 1..12 && anyo in 1995..2010){
-        if (tipoIva == "general"){
-            ivaGeneral = numero * 0.16f
-            println("${ivaGeneral+numero}")
-        }
-        if (tipoIva == "reduida"){
-            ivaReduido = numero * 0.07f
-            println("${ivaReduido+numero}")
-        }
-        if (tipoIva == "superreduit"){
-            ivaSuperreduido = numero * 0.04f
-            println("${ivaSuperreduido+numero}")
-        }
-        if (tipoIva == "exento"){
-            ivaExento = numero
-            println("${ivaExento+numero}")
-        }
-    }
-    else if (dia in 1..31 && mes in 1..12 && anyo in 2010..2012){
-        if (tipoIva == "general"){
-            ivaGeneral = numero * 0.18f
-            println("${ivaGeneral+numero}")
-        }
-        if (tipoIva == "reduida"){
-            ivaReduido = numero * 0.08f
-            println("${ivaReduido+numero}")
-        }
-        if (tipoIva == "superreduit"){
-            ivaSuperreduido = numero * 0.04f
-            println("${ivaSuperreduido+numero}")
-        }
-        if (tipoIva == "exento"){
-            ivaExento = numero
-            println("${ivaExento+numero}")
-        }
-    }
-    else if (dia in 17..31 && mes in 7..12 && anyo in 2012..3000){
-        if (tipoIva == "general"){
-            ivaGeneral = numero * 0.21f
-            println("${ivaGeneral+numero}")
-        }
-        if (tipoIva == "reduida"){
-            ivaReduido = numero * 0.10f
-            println("${ivaReduido+numero}")
-        }
-        if (tipoIva == "superreduit"){
-            ivaSuperreduido = numero * 0.04f
-            println("${ivaSuperreduido+numero}")
-        }
-        if (tipoIva == "exento"){
-            ivaExento = numero
-            println("${ivaExento+numero}")
-        }
-    }
-    else{
-        println("ERROR")
-    }
-}
 
-fun finalscan(scan: Scanner) {
-    scan.close()
+        "reduido" -> if (data in LocalDate.parse("01-01-1986", formatter)..LocalDate.parse("01-01-1995", formatter)) {
+            resultado = preu * 0.06f
+            println(resultado + preu)
+        } else if (data in LocalDate.parse("01-01-1995", formatter)..LocalDate.parse("01-01-2010", formatter)) {
+            resultado = preu * 0.07f
+            println(resultado + preu)
+        } else if (data in LocalDate.parse("01-01-2010", formatter)..LocalDate.parse("15-07-2012", formatter)) {
+            resultado = preu * 0.08f
+            println(resultado + preu)
+        } else if (data in LocalDate.parse("15-07-2012", formatter)..LocalDate.parse("01-01-3000", formatter))
+        else {
+            println("ERROR")
+        }
+
+        "general" -> if (data in LocalDate.parse("01-01-1986", formatter)..LocalDate.parse("01-01-1992", formatter)) {
+            resultado = preu * 0.12f
+            println(resultado + preu)
+        } else if (data in LocalDate.parse("01-01-1992", formatter)..LocalDate.parse("01-01-1995", formatter)) {
+            resultado = preu * 0.15f
+            println(resultado + preu)
+        } else if (data in LocalDate.parse("01-01-1995", formatter)..LocalDate.parse("01-01-2010", formatter)) {
+            resultado = preu * 0.16f
+            println(resultado + preu)
+        } else if (data in LocalDate.parse("01-01-2010", formatter)..LocalDate.parse("15-07-2012", formatter)) {
+            resultado = preu * 0.18f
+            println(resultado + preu)
+        } else if (data in LocalDate.parse("15-07-2012", formatter)..LocalDate.parse("01-01-3000", formatter)) {
+            resultado = preu * 0.21f
+            println(resultado + preu)
+        } else {
+            println("ERROR")
+        }
+    }
+    fun finalscan(scan: Scanner) {
+        scan.close()
+    }
 }
+//no se pueden hacer tests porque mi funcion del calculo del iva no devuelve naday hay ciertos parametros del calculo que no se pueden modificar
