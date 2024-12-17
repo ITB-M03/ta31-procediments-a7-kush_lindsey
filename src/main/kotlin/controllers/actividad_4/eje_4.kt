02af416ef9244884c03c90d2637ad95ea3654090
Lindsey López <lindsey.antunez7e8@itb.cat>
2024-12-17 21:24:56 +0100

    eje4_tests

--- src/main/kotlin/controllers/actividad_4/eje_4.kt
+++ src/main/kotlin/controllers/actividad_4/eje_4.kt
@@ -2,23 +2,45 @@ package controllers.actividad_4
 
 import org.example.controllers.*
 import java.util.*
+/**
+ * Programa que centra un mensaje en una pantalla de tamaño de 80 caracteres
+ * @param scan Scanner para leer por teclado
+ * @param mensaje variable que guarda el mensaje que escribio el usuario
+ * @author Lindsey Antunez
+ * @version 1.0
+ */
 fun main(){
     var scan = abrirScanner()
     var mensaje = pedirmensaje(scan)
     println(espacios(mensaje))
-
-
     cerrarScanner(scan)
 }
+
+/**
+ * funcion que solicita al usuario ingresar un mensaje
+ *
+ * @param scan Scanner utilizado para leer el mensaje del usuario.
+ * @return mensaje texto ingresado
+ */
 fun pedirmensaje(scan: Scanner):String{
     var mensaje: String
     print("Ingresa un mensaje: ")
     mensaje = scan.nextLine()
     return mensaje
 }
+
+/**
+ * centra el mensaje agregando espacios a la izquierda
+ * y derecha
+ *
+ * @param mensaje mensaje que se desea centrar.
+ * @return cadena con el mensaje centrado con espacios a los lados.
+ */
 fun espacios(mensaje: String): String {
+    var resultado : String
     val espaciosIzquierda = (80 - mensaje.length) / 2
     val espaciosDerecha = 80 - mensaje.length - espaciosIzquierda
-    return " ".repeat(espaciosIzquierda) + mensaje + " ".repeat(espaciosDerecha)
+    resultado = " ".repeat(espaciosIzquierda) + mensaje + " ".repeat(espaciosDerecha)
+    return resultado
 }
 
--- src/test/kotlin/controllers/actividad_4/Eje_4KtTest.kt
+++ src/test/kotlin/controllers/actividad_4/Eje_4KtTest.kt
@@ -7,10 +7,29 @@ import org.junit.jupiter.api.Assertions.*
 class Eje_4KtTest {
 
     @Test
-    fun espaciosCorrecto() {
+    fun testMensajeCorto() {
         val mensaje = "Hola"
-        val esperado = 80 - mensaje.length
+        val espaciosIzquierda = (80 - mensaje.length) / 2
+        val espaciosDerecha = 80 - mensaje.length - espaciosIzquierda
+        val esperado = " ".repeat(espaciosIzquierda) + mensaje + " ".repeat(espaciosDerecha)
         assertEquals(esperado, espacios(mensaje))
     }
+    @Test
+    fun testMensajeVacio() {
+        val mensaje = ""
+        val espaciosIzquierda = (80 - mensaje.length) / 2
+        val espaciosDerecha = 80 - mensaje.length - espaciosIzquierda
+        val esperado = " ".repeat(espaciosIzquierda) + "" + " ".repeat(espaciosDerecha)
+        assertEquals(esperado, espacios(mensaje))
+    }
+    @Test
+    fun testMensajeSoloEspacios() {
+        val mensaje = "     "
+        val espaciosIzquierda = (80 - mensaje.length) / 2
+        val espaciosDerecha = 80 - mensaje.length - espaciosIzquierda
+        val esperado = " ".repeat(espaciosIzquierda) + "     " + " ".repeat(espaciosDerecha)
+        assertEquals(esperado, espacios(mensaje))
+    }
+
 
 }
